b858cec21257b585225edf8ccb0276440d8b0f32
k-nasa <htilcs1115@gmail.com>
2019-07-23 18:55:19 +0900

    Add TokenType

---
+++ lexer/TokenType.java
@@ -0,0 +1,21 @@
+public final class TokenType {
+  public static final String ILLEGAL = "ILLEGAL";
+  public static final String EOF = "EOF";
+
+  public static final String IDENT = "IDENT";
+  public static final String INT = "INT";
+
+  public static final String ASSIGN = "=";
+  public static final String PLUS = "+";
+
+  public static final String COMMA = ",";
+  public static final String SEMICOLON = ";";
+
+  public static final String LPAREN = "(";
+  public static final String RPAREN = ")";
+  public static final String LBRACE = "{";
+  public static final String RBRACE = "}";
+
+  public static final String FUNCTION = "FUNCTION";
+  public static final String LET = "LET";
+}
